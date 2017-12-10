19c1fcb3b5bfba227dc499fddf844c792abc9c3b
Coleman McFarland <coleman.mcfarland@gmail.com>
2017-12-10 14:01:00 -0800

    cleanup combine test

--- backend/backend_test.go
+++ backend/backend_test.go
@@ -1,7 +1,6 @@
 package backend
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -9,7 +8,7 @@ func TestBackendData(t *testing.T) {
 
 	assert := func(l []string, r ...string) {
 		for i, x := range l {
-			fmt.Println(x, r[i])
+			t.Log(x, r[i])
 			if x != r[i] {
 				t.Errorf("expected %s got %s", x, r[i])
 			}
@@ -20,7 +19,7 @@ func TestBackendData(t *testing.T) {
 	b := []string{"10.2.1.56", "10.2.1.21"}
 
 	c := combine(a, b)
-	fmt.Println(c)
+	t.Logf("combined: %v", c)
 
 	assert(c, "10.2.1.20", "10.2.1.21", "10.2.1.56")
 }
